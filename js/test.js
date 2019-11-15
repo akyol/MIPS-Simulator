9f343b8ba7eb4f60651047f2e9269500a761a641
zondude <47907155+zondude@users.noreply.github.com>
2019-11-15 10:26:45 -0800

    Test and or load store

--- js/test.js
+++ js/test.js
@@ -4,8 +4,14 @@
  */
 
 $(window).on('load', function() {
-    addRegisterTest();
-    addImmediateTest();
+    // addRegisterTest();
+    // addImmediateTest();
+    // andRegisterTest();
+    // andImmediateTest();
+    // orRegisterTest();
+    // orImmediateTest();
+    // storeWordTest();
+    // loadWordTest();
 });
 
 
@@ -39,5 +45,116 @@ function addImmediateTest() {
 
     chooseOperation();
     console.log('The result is ' + getRegisterVal("registerOne"));
+}
+
+
+function andRegisterTest() {
+    
+    $("#filterOpcode").val("and");
+    $("#filterMode").val(""); // register: "", immediate: "i"
+    // $("#filterSigned").val(""); // signed: "", unsigned: "u"
+
+    $("#registerOne").val("v0");
+
+    $("#registerTwo").val("a0");
+    setRegisterVal("registerTwo", 5);
+
+    $("#registerThree").val("a1");
+    setRegisterVal("registerThree", 6);
+
+    chooseOperation();
+    console.log('The result is ' + getRegisterVal("registerOne"));
+}
+
+function andImmediateTest() {
+    $("#filterOpcode").val("and");
+    $("#filterMode").val("i"); // register: "", immediate: "i"
+    // $("#filterSigned").val(""); // signed: "", unsigned: "u"
+
+    $("#registerOne").val("v0");
+
+    $("#registerTwo").val("a0");
+    setRegisterVal("registerTwo", 6);
+
+    $("#immediate").val(12);
+
+    chooseOperation();
+    console.log('The result is ' + getRegisterVal("registerOne"));
+}
+
+function orRegisterTest() {
+    
+    $("#filterOpcode").val("or");
+    $("#filterMode").val(""); // register: "", immediate: "i"
+    // $("#filterSigned").val(""); // signed: "", unsigned: "u"
+
+    $("#registerOne").val("a0");
+
+    $("#registerTwo").val("v0");
+    setRegisterVal("registerTwo", 2);
+
+    $("#registerThree").val("v1");
+    setRegisterVal("registerThree", 3);
+
+    chooseOperation();
+    console.log('The result is ' + getRegisterVal("registerOne"));
+}
+
+function orImmediateTest() {
+    
+    $("#filterOpcode").val("or");
+    $("#filterMode").val("i"); // register: "", immediate: "i"
+    // $("#filterSigned").val(""); // signed: "", unsigned: "u"
+
+    $("#registerOne").val("a0");
+
+    $("#registerTwo").val("v0");
+    setRegisterVal("registerTwo", 2);
+
+    $("#immediate").val(5);
+
+    chooseOperation();
+    console.log('The result is ' + getRegisterVal("registerOne"));
+}
+
+function storeWordTest() {
+    $("#filterOpcode").val("sw");
+    // $("#filterMode").val("i"); // register: "", immediate: "i"
+    // $("#filterSigned").val(""); // signed: "", unsigned: "u"
+
+    $("#registerOne").val("zero");
+
+    $("#registerTwo").val("t0");
+    setRegisterVal("registerTwo", 6);
+
+    $("#immediate").val(0);
+
+    chooseOperation();
+    console.log('The result is ' + getRegisterVal("registerTwo") + ' stored in memory location [0]' );
+}
+
+function loadWordTest() {
+
+    $("#filterOpcode").val("sw");
+    // $("#filterMode").val("i"); // register: "", immediate: "i"
+    // $("#filterSigned").val(""); // signed: "", unsigned: "u"
+
+    $("#registerOne").val("zero");
+    $("#registerTwo").val("t0");
+    setRegisterVal("registerTwo", 6);
+    $("#immediate").val(0);
+    chooseOperation();
+
+
+    $("#filterOpcode").val("lw");
+    // $("#filterMode").val("i"); // register: "", immediate: "i"
+    // $("#filterSigned").val(""); // signed: "", unsigned: "u"
+    $("#registerOne").val("a0");
+    $("#registerTwo").val("zero");
+    // setRegisterVal("registerTwo", 6);
+    $("#immediate").val(0);
+
+    chooseOperation();
+    console.log('The value at memory locatoin 0 loaded into ' + getRegisterVal("registerOne"));
 }
 
